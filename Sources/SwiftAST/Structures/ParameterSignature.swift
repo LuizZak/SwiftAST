cbcb034433515a05111685194ad7ed98fbea7703
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2025-01-01 21:30:14 -0300

    Adding support for inout/borrowing/consuming parameter modifier

--- Sources/SwiftAST/Structures/ParameterSignature.swift
+++ Sources/SwiftAST/Structures/ParameterSignature.swift
@@ -3,6 +3,7 @@ public struct ParameterSignature: Hashable, Codable {
     public var name: String
     public var type: SwiftType
     public var isVariadic: Bool
+    public var modifier: Modifier
     // TODO: Support Expression as a default value.
     public var hasDefaultValue: Bool
 
@@ -11,21 +12,30 @@ public struct ParameterSignature: Hashable, Codable {
     ///
     /// Is equivalent to the default behavior of Swift parameters of creating a
     /// label with the same name if only a name is provided.
-    public init(name: String, type: SwiftType, isVariadic: Bool = false, hasDefaultValue: Bool = false) {
+    public init(name: String, type: SwiftType, modifier: Modifier = .none, isVariadic: Bool = false, hasDefaultValue: Bool = false) {
         self.label = name
         self.name = name
         self.type = type
+        self.modifier = modifier
         self.isVariadic = isVariadic
         self.hasDefaultValue = hasDefaultValue
     }
 
     /// Initializes a new parameter signature with a given set of values.
-    public init(label: String?, name: String, type: SwiftType, isVariadic: Bool = false, hasDefaultValue: Bool = false) {
+    public init(label: String?, name: String, type: SwiftType, modifier: Modifier = .none, isVariadic: Bool = false, hasDefaultValue: Bool = false) {
         self.label = label
         self.name = name
         self.type = type
+        self.modifier = modifier
         self.isVariadic = isVariadic
         self.hasDefaultValue = hasDefaultValue
+    }
+
+    public enum Modifier: String, Hashable, Codable {
+        case none
+        case `inout`
+        case borrowing
+        case consuming
     }
 }
 
@@ -42,6 +52,9 @@ extension ParameterSignature: CustomStringConvertible {
         }
 
         result += "\(name): "
+        if modifier != .none {
+            result += "\(modifier.rawValue) "
+        }
         result += type.description
 
         if isVariadic {
